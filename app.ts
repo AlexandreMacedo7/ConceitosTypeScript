520c444fec2fd7773b114e7e4382cb12a1bb44cb
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-07-07 14:59:30 -0400

    Utilizando array

--- app.ts
+++ app.ts
@@ -1,12 +1,15 @@
-//for
+//Array
 
-for(let i = 0; i < 5; i+=2){
-    console.log(i)
-}
+const array: Array<number> = [1,2,3,4,5]
+const arrayString: string[] = ['a','b','c','d']
 
-//while
-let n  = 0
-while(n < 5){
-    console.log(n)
-    n++
-}
+console.log(array[0])
+console.log(arrayString.length)
+
+console.log('Array original', array)
+array.push(7)
+console.log("Modificado ", array)
+
+array.pop()//remoção
+
+console.log("Utilizado pop - Array ", array)
