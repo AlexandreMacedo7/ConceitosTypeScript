bdbfb0bfd62f64aa4be858f7861efc72cac23326
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-07-14 19:11:20 -0400

    Implementa objetos

--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
-import { CompanyAccount } from "./CompanyAccount"
-import { PeopleAccount } from "./PeopleAccount"
+import { CompanyAccount } from "./CompanyAccount";
+import { PeopleAccount } from "./PeopleAccount";
+import { PersonalizedAccout } from './PersonalizedAccount';
 
 
 const account: PeopleAccount = new PeopleAccount(1,'Alexandre',7)
-account.deposit()
+account.deposit(100)
+account.whitdraw(100)
 
 
 const companyAccount: CompanyAccount = new CompanyAccount('A_Mac',2)
-companyAccount.deposit()
+companyAccount.deposit(1000)
+companyAccount.whitdraw(600)
+companyAccount.getLoan(100000)
+
+const personalized: PersonalizedAccout = new PersonalizedAccout('Mac', 2)
+personalized.deposit(100)
